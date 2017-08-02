2dbe90abbd1dbfb0c14db147d25354461dfeb7e1
John Emrick <emrickj248@comcast.net>
2017-08-02 16:02:13 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -4,8 +4,6 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width,initial-scale=1">
 <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <?php
    $name = $_POST['name'];
@@ -163,6 +161,8 @@ t1 { white-space: pre-wrap;}
       </div>
       <center><h6>This website was created using <a href="https://www.gem-editor.com">GEM</a>.</h6></center>
    </div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 </body>
 <script>
 
